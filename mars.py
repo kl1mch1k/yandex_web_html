f6db5ad8147195c29894f6e19533094e2b6edfa3
kl1mch1k <klimov.lenar@yandex.ru>
2022-02-09 15:24:23 +0300

    2

--- mars.py
+++ mars.py
@@ -13,5 +13,21 @@ def index():
     return "И на Марсе будут яблони цвести!"
 
 
+@app.route('/promotion')
+def promotion():
+    return '''<!doctype html>
+                        <html lang="en">
+                          <head>
+                            <meta charset="utf-8">
+                            <title> Mars </title>
+                          </head>
+                          <body>
+                            <p>Человечество вырастает из детства.</p>
+                            <p>Человечеству мала одна планета.</p>
+                            <p>Мы сделаем обитаемыми безжизненные пока планеты.</p>
+                            <p>И начнем с Марса!</p>
+                            <p>Присоединяйся!</p>'''
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
