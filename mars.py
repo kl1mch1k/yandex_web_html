6af99317dd2d792331259e712be03984358a37b6
kl1mch1k <klimov.lenar@yandex.ru>
2022-03-18 19:01:37 +0300

    6

--- mars.py
+++ mars.py
@@ -80,6 +80,38 @@ def promotion_image():
                                 </div>'''
 
 
+@app.route('/choice/<planet_name>')
+def planet_name(planet_name):
+    return f'''<!doctype html>
+                                <html lang="en">
+                                  <head>
+                                    <meta charset="utf-8">
+                                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                                    <link rel="stylesheet" 
+                                    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+                                    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                                    crossorigin="anonymous">
+                                    <title> Варианты выбора </title>
+                                  </head>
+                                  <body>
+                                    <h1> Мое предложение: {planet_name} </h1>
+                                    <div class="alert alert-dark" role="alert">
+                                    <h2> a </h2>
+                                    </div>
+                                    <div class="alert alert-success" role="alert">
+                                    <h2> a </h2>
+                                    </div>
+                                    <div class="alert alert-secondary" role="alert">
+                                    <h2> a </h2>
+                                    </div>
+                                    <div class="alert alert-warning" role="alert">
+                                    <h2> a </h2>
+                                    </div>
+                                    <div class="alert alert-danger" role="alert">
+                                    <h2> a </h2>
+                                    </div>'''
+
+
 @app.route('/astronaut_selection', methods=['POST', 'GET'])
 def form_sample():
     if request.method == 'GET':
